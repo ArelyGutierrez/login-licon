92ba9744ba4bcf8c81312d8adce27f0c487fbcc8
Arely Gutiérrez <yleraycnan@hotmail.com>
2023-01-21 02:32:55 -0600

    draft footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,34 @@
 import phoneIcon from '../images/phoneIcon.png';
+import locationIcon from '../images/locationIcon.png';
+import emailIcon from '../images/emailIcon.png'
+import GrupoLicon3 from '../images/GrupoLicon3.png'
+import banderaMex1 from '../images/banderaMex1.png'
 import './Footer.css';
 
-export const Footer = () =>{
-    return(
+export const Footer = () => {
+    return (
         <>
-            <footer id='loginFooter'>
-                <div>
+            <footer >
+                <div id='loginFooter'>
                     <section>Contacto</section>
-                     {/*<section>
-                    <img id='liconIcon' src={phoneIcon} alt={phoneIcon}/>
+                    <section id="icons">
+                        <img id='phoneIcon' src={phoneIcon} alt={phoneIcon} />
+                        <img id='locationIcon' src={locationIcon} alt={locationIcon} />
+                        <img id='emailIcon' src={emailIcon} alt={emailIcon} />
                     </section>
-                    <section>terminos y condiciones</section> */}
+                    <section className='lastSection'>
+                        <div id="terms">
+                            <p className='terms'>TÉRMINOS Y CONDICIONE- AVISO DE PRIVACIDAD POLÍTICA ANTI-SOBORNO</p>
+                        </div>
+                        <div className='iconsMex'>
+                            <p className='enterprise'>UNA EMPRESA DE</p>
+                            <img id='GrupoLicon3' src={GrupoLicon3} alt={GrupoLicon3} />
+                            <img id='banderaMex1' src={banderaMex1} alt={banderaMex1} />                            
+                        </div>
+                        <div>
+                            <p className='rights'>TODOS LOS DERECHOS RESERVADOS-2021</p>
+                        </div>
+                    </section>
                 </div>
             </footer>
         </>
